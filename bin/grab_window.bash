4dd8a30499e12aa802efd2d350efb558efbf32e5
Herman Lundkvist <herman.lundkvist@gmail.com>
2022-11-03 21:30:07 +0100

    Minor change.

--- bin/grab_window.bash
+++ bin/grab_window.bash
@@ -19,4 +19,4 @@ fi
 
 
 echo -en "\0use-hot-keys\x1ftrue\n"
-wmctrl -lx | sed -Ee 's/^([xa-f0-9]+) +([0-9]+) +[^\.]+\.([^ ]+)( +)[^ ]+ +(.+$)/\2 \3 \5 \x00icon\x1f\3\x1finfo\x1f\1\n/'
+wmctrl -lx | sed -Ee 's/^([xa-f0-9]+) +([0-9]+) +[^\.]+\.([^ ]+)( +)[^ ]+ +(.+$)/\2 \3 \5 \x00icon\x1f\L\3\E\x1finfo\x1f\1\n/'
